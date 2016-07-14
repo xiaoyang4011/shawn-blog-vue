c1e25217bf738ef870e8fb883f80a33827211813
Shawn <214118699@qq.com>
2016-07-14 21:40:23 +0800

    add lazyload

--- src/components/Home/cards.vue
+++ src/components/Home/cards.vue
@@ -1,30 +1,34 @@
 <template>
-  <div class="example-wrapper" id="example1">
-    <loadmore :top-method="loadTop" :bottom-method="loadBottom" :bottom-all-loaded="allLoaded">
+  <mt-header class="mint-header" fixed title="全部卡组">
+  <mt-button class="mint-button" v-link="'/'" slot="left" icon="back"></mt-button>
+  <mt-button slot="right"   @click="menu = true" icon="more">
+  </mt-button>
+  </mt-header>
+  <div class="center">
+    <mt-loadmore :bottom-method="loadBottom" :bottom-all-loaded="allLoaded">
       <ul class="index-ul">
         <li v-for="item in cardList" class="index-li">
-          <img :src="item.img" />
-          <div class="card_info">
-              <p>{{ item.name }}</p>
-          </div>
+              <img v-lazy="item.img"/>
+              <div class="card_info">
+                  <p>{{ item.name }}</p>
+              </div>
         </li>
       </ul>
-    </loadmore>
+    </mt-loadmore>
   </div>
+  <mt-popup :visible.sync="menu" position="right" class="mint-popup-3">
+
+  </mt-popup>
 </template>
 <script>
-require('mint-loadmore/lib/style.css')
 require('./../../assets/css/index.scss')
 import { getCardList } from '../../vuex/actions'
-import Loadmore from 'mint-loadmore'
 
 export default {
-  components: {
-    Loadmore
-  },
   data() {
     return {
-      limit : 20
+      menu : false,
+      limit : 0
     }
   },
   vuex:{
@@ -57,3 +61,38 @@ export default {
   }
 }
 </script>
+<style>
+.mint-header {
+  background-color: rgba(255, 255, 255, 0.95);
+  box-shadow: 0 0 4px rgba(0, 0, 0, 0.25);
+}
+.mint-header .mint-button {
+  color : silver;
+}
+.mint-header-title {
+  color: gray;
+  margin-top : 10px;
+  margin-bottom: 10px;
+}
+.mint-popup-3 {
+  width: 50%;
+  height: 100%;
+  background-color: #fff;
+}
+.index-li {
+  list-style-type: none;
+  text-align: center;
+  border-bottom: 1px solid #d5dbdb;
+  margin-top: 50px;
+  margin-bottom: 20px;
+}
+
+img[lazy=loading] {
+  width: 40px;
+  height: 40px;
+  margin: auto;
+  background-color: #ddd;
+  border-radius: 20px;
+}
+
+</style>
--- src/index.js
+++ src/index.js
@@ -6,10 +6,13 @@ import { sync } from 'vuex-router-sync'
 import store from './vuex/store'
 import configRouter from './routes'
 import App from './components/App.vue'
+import MintUI from 'mint-ui'
 import 'font-awesome/css/font-awesome.css'
 import 'bootstrap/dist/css/bootstrap.css'
 import 'vue-toast/dist/vue-toast.min.css'
+import 'mint-ui/lib/style.css'
 
+Vue.use(MintUI)
 Vue.use(VueRouter)
 Vue.use(VueValidator)
 
